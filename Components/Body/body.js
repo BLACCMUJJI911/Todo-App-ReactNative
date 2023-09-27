61e80a55566e7eb7fe4f8866e0f5b0d16f5b67ca
BLACCMUJJI911 <arieskm70@gmail.com>
2023-09-28 02:33:01 +0500

    updated

--- Components/Body/body.js
+++ Components/Body/body.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 // export default function Body() {
 
@@ -106,6 +106,7 @@ export default function Body() {
             </View>
             
 <View style={styles.listItem}>
+    <ScrollView>
 
                 {
 
@@ -115,7 +116,7 @@ export default function Body() {
 
                     ))
                 }
-
+</ScrollView>
             </View>
 
         </View>
